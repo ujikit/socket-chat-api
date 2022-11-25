71b94a949b6fe5e6ae2284d41bfd01bf00cd4f0e
ujikit <fauzizaki15@gmail.com>
2022-11-25 17:08:34 +0700

    fix: add handling for connection_error when connection is started from server

--- SocketIO.js
+++ SocketIO.js
@@ -59,12 +59,15 @@ io.on('connection', socket => {
 
   // [ERROR USER]
   io.engine.on('connection_error', err => {
-    console.log('==== connection_error ====');
-    console.log(err.req); // the request object
-    console.log(err.code); // the error code, for example 1
-    console.log(err.message); // the error message, for example "Session ID unknown"
-    console.log(err.context); // some additional error context
-    console.log('==== ./connection_error ====');
+    //
+    if (users.length) {
+      console.log('==== connection_error ====');
+      // console.log(err.req); // the request object
+      console.log(err.code); // the error code, for example 1
+      console.log(err.message); // the error message, for example "Session ID unknown"
+      console.log(err.context); // some additional error context
+      console.log('==== ./connection_error ====');
+    }
   });
 
   // [DISCONNECTED USER]
